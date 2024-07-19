4f5a7ee6d77b8df95abde0289539a36689ff4bde
DearPratipal <kumarpratipal123@gmail.com>
2024-07-19 17:34:25 +0530

    Declare4integer

---
+++ Module_3_Array/3.1_FundamentalOfArray/Worksheet1/Ques_05.cpp
@@ -0,0 +1,15 @@
+//Question 5: Declare and initialize an array of 4 integers with values 1, 2, 3, and 4 in one statement.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[4] = {1,2,3,4};
+    cout << arr[0] << endl;
+    cout << arr[1] << endl;
+    cout << arr[2] << endl;
+    cout << arr[3] << endl;
+
+    return 0;
+}
