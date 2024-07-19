fddf8fe560d57a23ec4bd5455b8483cce9b11e3c
DearPratipal <kumarpratipal123@gmail.com>
2024-07-19 17:28:45 +0530

    ques4complete

---
+++ Module_3_Array/3.1_FundamentalOfArray/July_2024.cpp
@@ -0,0 +1,18 @@
+/*
+
+What is an array? Syntax and Declaration? How to access Elements in Array ? Printing Output and Taking Input?
+
+*/
+
+/*
+An array is a type of data structure used to store a group or collection of items or elements
+sequentially in memory.
+• The main function of an array is to store a collection of homogenous data of the same type
+(e.g., integers, strings, floating numbers).
+• Arrays use 0-based indexing, meaning the first element is at index 0, the second element is at
+index 1, and so on.
+• Elements can be directly accessed using their indexes.
+• Memory allocation in arrays is contiguous in nature, allowing for efficient data management
+and access.
+
+*/
---
+++ Module_3_Array/3.1_FundamentalOfArray/Worksheet1/Ques_04.cpp
@@ -0,0 +1,21 @@
+
+
+// Question 4: Initialize an array of 3 characters with values 'a', 'b', and 'c'.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char arr[3];
+    arr[0] = 'a';
+    arr[1] = 'b';
+    arr[2] = 'c';
+    
+    
+    cout<<arr[0]<<endl;
+    cout<<arr[1]<<endl;
+    cout<<arr[2]<<endl;
+    
+    return 0;
+}
