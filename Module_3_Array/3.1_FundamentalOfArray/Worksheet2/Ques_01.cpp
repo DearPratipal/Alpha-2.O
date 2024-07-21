aba90d6ecd35ae4dc487b4c2b3107c261b96abb8
DearPratipal <kumarpratipal123@gmail.com>
2024-07-21 10:25:10 +0530

    DoneQues

---
+++ Module_3_Array/3.1_FundamentalOfArray/Worksheet2/Ques_01.cpp
@@ -0,0 +1,23 @@
+//Calculate the product of all the elements in the given array.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the size of Array : ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"Enter the "<<i+1<<"th"<<" element of Array : ";
+        cin>>arr[i];
+        }
+        int product=1;
+        for(int i=0;i<n;i++){
+            product=product*arr[i];
+            }
+            cout<<product;
+
+            return 0;
+            
+}
